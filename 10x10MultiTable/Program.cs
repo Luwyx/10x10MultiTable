844e82040fd8a48984795c41aa24a177513abb37
Gayvery <lokelindolsen@gmail.com>
2023-08-07 11:09:37 +0200

    Add project files.

---
+++ 10x10MultiTable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10x10MultiTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
